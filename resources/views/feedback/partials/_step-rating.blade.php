6deb801a5c5491098120fc4b95781cd999d6c792
amit2x <gate.amitkumarsingh@gmail.com>
2026-09-26 17:40:50 +0530

    Content Fix

--- resources/views/feedback/partials/_step-rating.blade.php
+++ resources/views/feedback/partials/_step-rating.blade.php
@@ -2,11 +2,16 @@
     <h2 class="paf-step-title">{{ __('messages.how_was_overall') }}</h2>
 
     <div class="paf-rating mb-2" role="radiogroup" aria-label="{{ __('messages.how_was_overall') }}">
-        <button type="button" class="paf-rating-button" data-rating="1" aria-label="{{ __('messages.rating.1') }}">😡</button>
-        <button type="button" class="paf-rating-button" data-rating="2" aria-label="{{ __('messages.rating.2') }}">😞</button>
-        <button type="button" class="paf-rating-button" data-rating="3" aria-label="{{ __('messages.rating.3') }}">😐</button>
-        <button type="button" class="paf-rating-button" data-rating="4" aria-label="{{ __('messages.rating.4') }}">🙂</button>
-        <button type="button" class="paf-rating-button" data-rating="5" aria-label="{{ __('messages.rating.5') }}">😍</button>
+        <button type="button" class="paf-rating-button" data-rating="1"
+            aria-label="{{ __('messages.rating.1') }}">😡</button>
+        <button type="button" class="paf-rating-button" data-rating="2"
+            aria-label="{{ __('messages.rating.2') }}">😞</button>
+        <button type="button" class="paf-rating-button" data-rating="3"
+            aria-label="{{ __('messages.rating.3') }}">😐</button>
+        <button type="button" class="paf-rating-button" data-rating="4"
+            aria-label="{{ __('messages.rating.4') }}">🙂</button>
+        <button type="button" class="paf-rating-button" data-rating="5"
+            aria-label="{{ __('messages.rating.5') }}">😍</button>
     </div>
 
     <div class="paf-rating-labels">
